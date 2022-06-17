f87ade74abfcf6e1aee41ca16849554323f2901c
Sofia <misofser@gmail.com>
2022-06-17 20:38:33 +0300

    create new JS function

---
+++ 1.js
@@ -0,0 +1,11 @@
+function comparing(a, b) {
+    if (a > b) {
+      console.log('a is bigger than b')
+    } else if (b > a) {
+      console.log('b is bigger than a')
+    } else {
+      console.log('numbers are eaquel');
+    }
+};
+
+comparing(9, 9);
